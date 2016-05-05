3e9c5286e3ba48f509d9ffa7cd7f40196b09761c
Student Temp Account <student@macadmins-iMac-46.local>
2016-05-05 16:01:35 -0400

    Mouth

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+	
+	$("#out").click(function(){
+	$("#mouth_image").toggle();
+	
+	})
+	$("#in").click(function(){
+		var color = ["Yellow", "Blue", "Green", "Black"]
+	var random = Math.floor(Math.random() * color.length)
+	
+		$("body").css("background-color", color[random])
+	})
+	$("#keys").click(function(){
+		$("#potato").toggleClass("jumpy")
+	});
 
+	
+});
